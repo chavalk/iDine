f5d8d1219847b2dcbd8d5a02e0ea2e87dd46a0ad
chavalk <chavalk@hotmail.com>
2023-05-08 21:03:34 -0500

    First commit.

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Jose Garcia on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
